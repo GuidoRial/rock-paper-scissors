41f3e8e482e0e188c0d2322ae00bb2c34bdd64bb
GuidoRial <rialguido@gmail.com>
2021-10-05 14:19:28 -0300

    I made it work, but it counts up to 8 points each and doesn't display the last message

--- java.js
+++ java.js
@@ -1,7 +1,6 @@
 //I'll need a few functions, a computerSelection, a playerSelection that's case sensitive, both will make a round, five of those will make a game//
 
 /*Introduction to the game
-
 let firstIntroduction = alert("The game is simple, win 5 rounds to win a game")
 let secondIntroduction = alert("May the better person win. Good luck")
 let thirdIntroduction = alert("El que pierde es trolo")
@@ -16,9 +15,7 @@ function computerSelection () {
     return computerPick;
 }
 
-
 //Function that allows the player to write their choise, as long as they spell correctly, their pick will be changed to lower case and then compared to the computerPick//
-
 
 function playerSelection () {
     let playerQuestion = prompt("What do you choose?");
@@ -27,16 +24,10 @@ function playerSelection () {
     return playerPick;
 }
 
-
 //Function that covers a whole round, it lets you know if you win or lose, and also if you wrote something wrong.
 
-
-
-
-
 function round () {
-    let playerScore = 0;
-    let computerScore = 0;
+
     let playerPick = playerSelection();
     let computerPick = computerSelection();
 
@@ -58,14 +49,13 @@ function round () {
             alert("Invalid input");
             }
 
-
         if ((playerPick === "scissors" && computerPick === "paper") || (playerPick === "paper" && computerPick === "rock") || (playerPick === "rock" && computerPick === "scissors")){
-            console.log("Nice one, you made " + playerScore + " point!");
+            console.log("Nice one, you have " + playerScore + " point!");
 
         }
 
         else if ((computerPick === "scissors" && playerPick === "paper") || (computerPick === "paper" && playerPick === "rock") || (computerPick === "rock" && playerPick === "scissors")){
-            console.log("The computer made " + computerScore + " point");
+            console.log("The computer has " + computerScore + " point");
 
         }
 
@@ -74,27 +64,20 @@ function round () {
         }
         console.log("Player points: " + playerScore);
         console.log("Computer points: " + computerScore);
-
     }
 
-function game() {
+    
     let playerScore = 0;
     let computerScore = 0;
-    for (let i = 0; i < 5; i++)
 
-
-
-    round();
-    if (playerScore == 5) {
-        console.log("You won the game, congrats!");
-        playerScore = 0;
-        computerScore = 0;
+    while ((computerScore < 5) || (playerScore <5)) {
+        let playerScore = 0;
+        let computerScore = 0;
+        round();
+        if (playerScore == 5) {
+            console.log("You won the game, congrats!");
+        }
+        else if (computerScore == 5) {
+            console.log("You suck at this, go do something else");
+        }
     }
-    else if (computerScore == 5) {
-        console.log("You suck at this, go do something else");
-        playerScore = 0;
-        computerScore = 0;
-    }
-}
-
-game();
