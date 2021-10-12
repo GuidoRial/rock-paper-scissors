089e10006dd8d748ce61f50b35d7d94492bb5a1d
GuidoRial <rialguido@gmail.com>
2021-10-12 15:46:17 -0300

    added UI and updated the readme

--- README.md
+++ README.md
@@ -1,10 +1,12 @@
 # rock-paper-scissors
 ## General info
-    This project is a simple rock paper scissors game. I did it as part of The Odin Project curriculum and it was my first project in JavaScript
+    This project is a simple rock paper scissors game. I did it as part of The Odin Project curriculum and it was my first project in JavaScript.
+    When I finish the logic behind it I added the UI with HTML and CSS, but copied what I saw in a tutorial from FreeCodeCamp (except from the colors)
 
 ## What I used
+  *  HTML
+  *  CSS
   *  JavaScript
-  *  I plan on adding HTML and CSS later on
 
 ## What I learned
   * JavaScript
@@ -15,4 +17,9 @@
     -  Conditionals
     -  Logical Operators
     -  Functions
+    -  That I have to put the script tag at the end of the body to avoid issues with JS
+  * CSS
+    - How to import fonts from font.google.com
+  * HTML
+    - How to structure a webpage correctly
 
---
+++ app.js
@@ -0,0 +1,97 @@
+/*Introduction to the game
+let firstIntroduction = alert("The game is simple, win 5 rounds to win a game")
+let secondIntroduction = alert("May the better person win. Good luck")
+let thirdIntroduction = alert("Press F12 and go to console")
+*/
+
+//Function that picks randomly between the elements of an Array//
+
+let userScore = 0;
+let computerScore = 0;
+const userScore_span = document.getElementById("user-score");
+const computerScore_span = document.getElementById("computer-score");
+const scoreBoard_div = document.querySelector(".score-board");
+const result_p = document.querySelector(".result > p");
+const rock_div = document.getElementById("rock");
+const paper_div = document.getElementById("paper");
+const scissors_div = document.getElementById("scissors");
+
+function computerSelection () {
+    const computerChoises = ["rock", "paper", "scissors"];
+    let computerPick = computerChoises[Math.floor(Math.random() * computerChoises.length)];
+    return computerPick;
+}
+
+function win (userChoice, computerChoice) {
+    userScore++;
+    userScore_span.innerHTML = userScore;
+    computerScore_span.innerHTML = computerScore;
+    result_p.innerHTML = "You win this round!";
+}
+
+function lose (userChoice, computerChoice) {
+    computerScore++
+    userScore_span.innerHTML = userScore;
+    computerScore_span.innerHTML = computerScore;
+    result_p.innerHTML = "You lost this round!";
+}
+
+function draw (userChoice, computerChoice) {
+    result_p.innerHTML = "It\'s a tie!"
+}
+
+function winGame () {
+    userScore = 0;
+    computerScore = 0;
+    result_p.innerHTML = "You won the game!";    
+}
+
+function loseGame () {
+    userScore = 0;
+    computerScore = 0;
+    result_p.innerHTML = "You lost the game!";    
+}
+
+function game(userChoice) {
+    const computerChoice = computerSelection();
+    switch (userChoice + computerChoice) {
+        case "rockscissors":
+        case "paperrock":
+        case "scissorspaper":
+            win();
+            break;
+        case "rockpaper":
+        case "paperscissors":
+        case "scissorsrock":
+            lose();
+            break;
+        case "rockrock":
+        case "paperpaper":
+        case "scissorsscissors":
+            draw();
+            break;      
+    }
+    if (computerScore == 5) {
+        loseGame();
+    }
+    else if (userScore == 5) {
+        winGame();
+    }    
+}
+
+function main() {
+    rock_div.addEventListener('click', function() {
+        game("rock");
+    })
+
+    paper_div.addEventListener('click', function() {
+        game("paper");
+    })
+
+    scissors_div.addEventListener('click', function() {
+        game("scissors");
+    })
+}
+
+main();
+
